07e77e08df1a3f0716cfcebcfcfbc747f2c4b65d
Manolya Tamimi <tamimimanolya@gmail.com>
2023-10-09 11:22:44 +0300

    rendering items in view page

--- src/pages/view/view.jsx
+++ src/pages/view/view.jsx
@@ -1,9 +1,20 @@
 import './view.css';
+import { shoppingItems } from '../../data/shoppingItems'
+import ItemCard from '../../components/item-card/item-card';
 
 const ViewPage = () => {
     return (
         <div className='view-page'>
-            
+            {shoppingItems.map((item) => {
+                return (
+                    <ItemCard
+                        name={item.name}
+                        description={item.description}
+                        price={item.price}
+                        imgUrl={item.imageUrl}
+                    />
+                )
+            })}
         </div>
     )
 }
