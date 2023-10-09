9f170e4e04f9966ac0e2adbee41542f7a86e1fd9
Manolya Tamimi <tamimimanolya@gmail.com>
2023-10-09 11:21:38 +0300

    adding images

--- src/data/shoppingItems.js
+++ src/data/shoppingItems.js
@@ -4,209 +4,132 @@ export const shoppingItems = [
         name: "Smartphone",
         description: "A high-end smartphone with the latest features",
         price: 999.99,
-        imageUrl: ""
+        imageUrl: "https://www.bell.ca/Styles/images/iPhone13/iPhone_13_Midnight_5G-plus_lrg1.png"
     },
     {
         id: 2,
         name: "Laptop",
         description: "A powerful laptop for work and entertainment",
         price: 1499.99,
-        imageUrl: ""
+        imageUrl: "https://promart.vteximg.com.br/arquivos/ids/7059304-1000-1000/image-0.jpg?v=638206840729730000"
     },
     {
         id: 3,
         name: "Headphones",
         description: "Noise-cancelling headphones for immersive audio",
         price: 299.99,
-        imageUrl: ""
+        imageUrl: "https://www.beatsbydre.com/content/dam/beats/web/product/headphones/studiopro-wireless/global/serp/studiopro-pdp-global-serp-black.jpg"
     },
     {
         id: 4,
         name: "Smartwatch",
         description: "A sleek smartwatch with fitness tracking features",
         price: 399.99,
-        imageUrl: ""
+        imageUrl: "https://prod.radley.co.uk/media/catalog/product/r/y/rys06-2080_1.jpg?optimize=medium&fit=bounds&height=&width=?width=300"
     },
     {
         id: 5,
         name: "Camera",
         description: "A professional-grade camera for capturing stunning photos",
         price: 1999.99,
-        imageUrl: ""
+        imageUrl: "https://in.canon/media/image/2023/02/06/c83adfc2fcaf473f9935b6962f114b45_EOS+R50+Black+RF-S18-45mm.png"
     },
     {
         id: 6,
         name: "Gaming PC",
         description: "A high-performance gaming PC for the ultimate gaming experience",
         price: 2999.99,
-        imageUrl: ""
+        imageUrl: "https://i.ebayimg.com/images/g/q2wAAOSwy4Jk0g-B/s-l1600.jpg"
     },
     {
         id: 7,
         name: "Tablet",
         description: "A versatile tablet for work and entertainment on-the-go",
         price: 799.99,
-        imageUrl: ""
+        imageUrl: "https://www.reliancedigital.in/medias/Samsung-Galaxy-TabS6-Tablet-493177064-i-6-1200Wx1200H?context=bWFzdGVyfGltYWdlc3w1Nzk2NXxpbWFnZS9qcGVnfGltYWdlcy9oMzkvaDNmLzk4NjQzOTc4NDg2MDYuanBnfGY4MjliMmI3MjRjYWJlMzIxODM3MDc4ZWU4NGE0Mjk1ZjE1Y2M2OGU5N2FiYjFkZGY2YTdmZDA4OTZjNjcwMzM"
     },
     {
         id: 8,
         name: "Wireless Earbuds",
         description: "True wireless earbuds with long battery life and great sound quality",
         price: 149.99,
-        imageUrl: ""
+        imageUrl: "https://m.media-amazon.com/images/I/61ERSWgDaOL.jpg"
     },
     {
         id: 9,
         name: "Smart Speaker",
         description: "A voice-controlled smart speaker with built-in virtual assistant",
         price: 199.99,
-        imageUrl: ""
+        imageUrl: "https://media.gq-magazine.co.uk/photos/63eccf7d9b06869e196c6571/3:4/w_748%2Cc_limit/SmartSpeaker_Apple-HomePod-2--.png.jpg"
     },
     {
         id: 10,
         name: "Drone",
         description: "A high-quality drone for aerial photography and videography",
         price: 1299.99,
-        imageUrl: ""
-    },
-    {
-        id: 1,
-        name: "Smartphone",
-        description: "A high-end smartphone with the latest features",
-        price: 999.99,
-        imageUrl: ""
-    },
-    {
-        id: 2,
-        name: "Laptop",
-        description: "A powerful laptop for work and entertainment",
-        price: 1499.99,
-        imageUrl: ""
-    },
-    {
-        id: 3,
-        name: "Headphones",
-        description: "Noise-cancelling headphones for immersive audio",
-        price: 299.99,
-        imageUrl: ""
-    },
-    {
-        id: 4,
-        name: "Smartwatch",
-        description: "A sleek smartwatch with fitness tracking features",
-        price: 399.99,
-        imageUrl: ""
-    },
-    {
-        id: 5,
-        name: "Camera",
-        description: "A professional-grade camera for capturing stunning photos",
-        price: 1999.99,
-        imageUrl: ""
-    },
-    {
-        id: 6,
-        name: "Gaming PC",
-        description: "A high-performance gaming PC for the ultimate gaming experience",
-        price: 2999.99,
-        imageUrl: ""
-    },
-    {
-        id: 7,
-        name: "Tablet",
-        description: "A versatile tablet for work and entertainment on-the-go",
-        price: 799.99,
-        imageUrl: ""
-    },
-    {
-        id: 8,
-        name: "Wireless Earbuds",
-        description: "True wireless earbuds with long battery life and great sound quality",
-        price: 149.99,
-        imageUrl: ""
-    },
-    {
-        id: 9,
-        name: "Smart Speaker",
-        description: "A voice-controlled smart speaker with built-in virtual assistant",
-        price: 199.99,
-        imageUrl: ""
-    },
-    {
-        id: 10,
-        name: "Drone",
-        description: "A high-quality drone for aerial photography and videography",
-        price: 1299.99,
-        imageUrl: ""
+        imageUrl: "https://stormsend1.djicdn.com/tpc/uploads/spu/three_d_cover/fb1831949f59aba5793fc851036f016f@ultra.png"
     },
     {
         id: 11,
         name: "Smart TV",
         description: "A large-screen smart TV with 4K resolution and HDR support",
         price: 1999.99,
-        imageUrl: ""
+        imageUrl: "https://www.sencor.com/Sencor/media/content/Products/SLE-55US800TCSB-2.jpg"
     },
     {
         id: 12,
         name: "Fitness Tracker",
         description: "A wearable fitness tracker with heart rate monitoring and GPS",
         price: 149.99,
-        imageUrl: ""
+        imageUrl: "https://digitalwalker.ph/cdn/shop/products/FITBITLUXEBLACKGRAPHITE1_1_1000x1000.png?v=1662511450"
     },
     {
         id: 13,
-        name: "Electric Bike",
-        description: "A high-performance electric bike for commuting and leisure",
-        price: 2999.99,
-        imageUrl: ""
-    },
-    {
-        id: 14,
         name: "Virtual Reality Headset",
         description: "A cutting-edge VR headset for immersive gaming and entertainment",
         price: 799.99,
-        imageUrl: ""
+        imageUrl: "https://cdn.pricespy.co.nz/product/standard/280/5526645.jpg"
     },
     {
-        id: 15,
+        id: 14,
         name: "Wireless Charger",
         description: "A fast wireless charger for smartphones and other devices",
         price: 49.99,
-        imageUrl: ""
+        imageUrl: "https://m.media-amazon.com/images/I/31Wg3xvQCTL.jpg"
     },
     {
-        id: 16,
+        id: 15,
         name: "Portable Bluetooth Speaker",
         description: "A compact and portable Bluetooth speaker with great sound quality",
         price: 99.99,
-        imageUrl: ""
+        imageUrl: "https://i5.walmartimages.com/seo/JBL-Pulse-4-Waterproof-Portable-Bluetooth-Speaker-with-Light-Show-and-Sound-Black_f8ff7cd1-a7f8-4285-ba4d-d716cff3230e_2.3506a8103048cbc7b7a19f3d030f7af1.jpeg"
     },
     {
-        id: 17,
+        id: 16,
         name: "Smart Thermostat",
         description: "A programmable smart thermostat for efficient home heating and cooling",
         price: 249.99,
-        imageUrl: ""
+        imageUrl: "https://media-cldnry.s-nbcnews.com/image/upload/t_fit-720w,f_auto,q_auto:best/newscms/2023_27/3613772/31hgsk5am0l-sl500-64a2d91720441.jpg"
     },
     {
-        id: 18,
+        id: 17,
         name: "Robot Vacuum",
         description: "A smart robot vacuum for automated cleaning of floors and carpets",
         price: 499.99,
-        imageUrl: ""
+        imageUrl: "https://ae01.alicdn.com/kf/Sb0c86faa4a514add9cf3a7513866873aF/Geekroom-Floor-Robot-Vacuum-Cleaner-Sweep-and-Wet-Mopping-Floors-Carpet-Run-120-min-Auto-Recharge.jpg"
     },
     {
-        id: 19,
+        id: 18,
         name: "Wireless Gaming Mouse",
         description: "A high-performance wireless gaming mouse with customizable buttons",
         price: 99.99,
-        imageUrl: ""
+        imageUrl: "https://m.media-amazon.com/images/I/61I8BjnzubL.jpg"
     },
     {
-        id: 20,
+        id: 19,
         name: "Smart Lock",
         description: "A keyless smart lock for secure and convenient home access",
         price: 199.99,
-        imageUrl: ""
+        imageUrl: "https://m.media-amazon.com/images/I/81DMJUIPgTL.jpg"
     }
 ];
