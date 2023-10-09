1d02b420e59d4f6fd58372b757ae7560e3be55d6
Manolya Tamimi <tamimimanolya@gmail.com>
2023-10-09 16:02:31 +0300

    adding empty cart indecation

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -7,22 +7,26 @@ const CartPage = () => {
     console.log(cart);
     return (
         <div className='cart-page'>
-            {cart.map((item) => {
-                return (
-                    <ItemCard
-                        key={'viewpage_itemCard' + item.id}
-                        name={item.name}
-                        description={item.description}
-                        price={item.price}
-                        imgUrl={item.imageUrl}
-                        cartBtnOnClick={() => {
-                            const newCart = cart.filter((tmp) => tmp !== item);
-                            setCart(newCart);
-                        }}
-                        cartBtnText={'Remove from cart'}
-                    />
-                )
-            })}
+            {
+                cart.length ?
+                    cart.map((item) => {
+                        return (
+                            <ItemCard
+                                key={'viewpage_itemCard' + item.id}
+                                name={item.name}
+                                description={item.description}
+                                price={item.price}
+                                imgUrl={item.imageUrl}
+                                cartBtnOnClick={() => {
+                                    const newCart = cart.filter((tmp) => tmp !== item);
+                                    setCart(newCart);
+                                }}
+                                cartBtnText={'Remove from cart'}
+                            />
+                        )
+                    }
+                    ) : <h4>your cart is empty -_-</h4>
+            }
         </div>
     )
 }
